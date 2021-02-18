336c06ca2fe59ceadb78289c599a8dcde633597a
Agus Priyatin <aguspriyatin80>
2021-02-18 10:26:37 +0700

    delete all

--- src/main/java/com/glints/librarymanagement/repository/AuthorRepo.java
+++
@@ -1,9 +0,0 @@
-package com.glints.librarymanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.glints.librarymanagement.model.Author;
-
-public interface AuthorRepo extends JpaRepository<Author, Integer> {
-	Author findByFirstnameIgnoreCase(String name);
-}
